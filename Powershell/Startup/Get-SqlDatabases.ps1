8772c2309800f54d3821407997f1fbd1b0394d20
Wolfgang Puchinger <wpuchinger@rocom.de>
2022-10-25 15:06:27 +0200

    Changed coloring to use ps1xml

--- Powershell/Startup/Get-SqlDatabases.ps1
+++ Powershell/Startup/Get-SqlDatabases.ps1
@@ -1,32 +1,20 @@
+[cmdletbinding()]
 param (
-    [Parameter(Mandatory=$false,
-               ParameterSetName="ParameterSetName")]
-    [switch]
-    $Plain
 )
 begin {
+    if ($null -eq (Get-FormatData -TypeName 'User.Database')) {
+        Update-FormatData -PrependPath "$PSScriptRoot\..\Format\User.Database.ps1xml"
+    }
 }
 process {
     Invoke-Sqlcmd -Database master -Query "SELECT name, compatibility_level, state_desc FROM sys.databases ORDER BY name" |
         ForEach-Object {
-            [PSCustomObject]@{
+            $object = [pscustomobject]@{
                 name= $_.name.ToString()
                 compatibility_level= $_.compatibility_level.ToString()
                 state_desc= $_.state_desc.ToString()
             }
-        } |
-        ForEach-Object {
-            $object = $_
-
-            if (!$Plain) {
-                if ($object.name -eq $($PSDefaultParameterValues["*:Database"])) {
-                    foreach ($prop in $object | Get-Member -MemberType NoteProperty) 
-                    {
-                        $object.($prop.name) = "`e[38;5;2m$($object.($prop.name))`e[0m"
-                    }
-                }
-            }
-
+            $object.PSObject.TypeNames.Insert(0,'User.Database')
             $object
         }
 }
