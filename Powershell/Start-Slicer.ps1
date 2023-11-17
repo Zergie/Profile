c4489144c7ec2f0ea6f885672f53baaa20fe4c2a
Wolfgang Puchinger <wpuchinger@rocom.de>
2023-11-17 06:33:20 +0100

    Updated Profile

--- Powershell/Start-Slicer.ps1
+++ Powershell/Start-Slicer.ps1
@@ -1,4 +1,5 @@
 $slicer = "C:\ProgramData\chocolatey\lib\orcaslicer\tools\orca-slicer.exe"
+$slicer_process = Get-Process ([System.IO.Path]::GetFileNameWithoutExtension($slicer))
 
 $path = $args
 $filename = [System.IO.Path]::GetFileNameWithoutExtension($path)
@@ -11,5 +12,15 @@ $Destination = "${env:USERPROFILE}\Downloads\${filename}_$((Get-Date).ToString("
     Destination = $Destination
 }
 Copy-Item -Path "${path}" -Destination "${Destination}" -Force
+
+if ($null -ne $slicer_process) {
+    $wshell = New-Object -ComObject wscript.shell;
+    $wshell.AppActivate($slicer_process.MainWindowTitle)
+    $wshell.SendKeys("^i")
+    $wshell.SendKeys($Destination)
+    $wshell.SendKeys("{Enter}")
+} else {
+    . $slicer "${Destination}"
+}
 
 #pause
