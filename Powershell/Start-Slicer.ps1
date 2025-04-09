14d921558b359aee437c601f8ef0714f2d8d6ab3
Wolfgang Puchinger <wpuchinger@rocom.de>
2025-04-09 09:57:33 +0200

    Refactor Start-Slicer.ps1 to include enhanced logging, dynamic port allocation, and error handling.

--- Powershell/Start-Slicer.ps1
+++ Powershell/Start-Slicer.ps1
@@ -1,38 +1,69 @@
-$item = Get-ChildItem ("$args*" -replace '(\[|\])','``$1') | Sort-Object LastWriteTime | Select-Object -Last 1
-Push-Location $item.Directory
-$file = $item.Name
-
-$Debug = $false
-$Debug = $Debug -or $path.Length -eq 0
-if ($Debug) {
-    Write-Host -ForegroundColor Yellow "`$args=" -NoNewline
-    $args | ConvertTo-Json | Write-Host -ForegroundColor Yellow
-    Write-Host -ForegroundColor Yellow "`$Location=$(Get-Location)"
-    Write-Host -ForegroundColor Yellow "`$file=$file"
+$exception = $null
+$conversation = @()
+function Write-Log {
+    [ordered]@{
+        date         = (Get-Date).ToString()
+        args         = $args
+        conversation = $conversation
+        exception    = @{
+            FullyQualifiedErrorId = $exception.FullyQualifiedErrorId
+            Message               = $exception.Exception.ErrorRecord.Exception.Message
+            PositionMessage       = $exception.InvocationInfo.PositionMessage
+        }
+    } |
+        ConvertTo-Json -Depth 2 |
+        Set-Content "$env:TEMP\Start-Slicer.log" -Force
 }
+try {
+    $item = Get-ChildItem ("$args*" -replace '(\[|\])','``$1') | Sort-Object LastWriteTime | Select-Object -Last 1
+    Push-Location $item.Directory
+    $file = $item.Name
 
-$http = [System.Net.HttpListener]::new()
-$http.Prefixes.Add("http://localhost:8080/")
-$http.Start()
-if ($http.IsListening) {
-    Start-Process "orcaslicer://open?file=$([System.Web.HttpUtility]::UrlEncode("http://localhost:8080/$file"))"
+    Write-Log
+    Add-Type -AssemblyName System.Web
+
+    foreach ($try in 0..5) {
+        $http = [System.Net.HttpListener]::new()
+        $port = Get-Random -Minimum 49152 -Maximum 65535
+        $http.Prefixes.Add("http://localhost:$port/")
+        try {
+            Write-Host "Starting on port $port"
+            $http.Start()
+            break;
+        } catch { }
+    }
+    if (!$http.IsListening) {
+        $http.Start()
+    }
+    if ($http.IsListening) {
+        Write-Host "IsListening on port $port"
+        $url = "orcaslicer://open?file=$([System.Web.HttpUtility]::UrlEncode("http://localhost:$port/file.stl"))"
+        $url = "orcaslicer://open?file=$("http://localhost:$port/$file.stl")"
+        Write-Host "Starting '$url'"
+        Start-Process $url
+    }
+    $context = $http.GetContext()
+    Write-Host ">> $($context.Request.HttpMethod) $($context.Request.Url)"
+    $conversation += ">> $($context.Request.HttpMethod) $($context.Request.Url)"
+    $fs = [System.IO.File]::OpenRead($item.FullName)
+    $context.Response.ContentLength64 = $fs.Length
+    $context.Response.SendChunked = $false
+    $context.Response.ContentType = "application/octet-stream"
+    $context.Response.AddHeader("Content-disposition", "attachment; filename=" + $file)
+    $fs.CopyTo($context.Response.OutputStream)
+    $context.Response.StatusCode = 200
+    $context.Response.StatusDescription = "Ok"
+    Write-Host "<< $($context.Response.StatusCode) $($context.Response.StatusDescription)"
+    $conversation += "<< $($context.Response.StatusCode) $($context.Response.StatusDescription)"
+    $context.Response.OutputStream.Close()
+    Start-Sleep -Seconds 10
+    $http.Stop()
+    $http.Close()
+    $fs.Dispose()
+    $http.Dispose()
+    Pop-Location
+} catch {
+    $exception = $_
+} finally {
+    Write-Log
 }
-$context = $http.GetContext()
-Write-Host -ForegroundColor Magenta ">> $($context.Request.HttpMethod) $($context.Request.Url)"
-$fs = [System.IO.File]::OpenRead((Resolve-Path $file).Path)
-$context.Response.ContentLength64 = $fs.Length
-$context.Response.SendChunked = $false
-$context.Response.ContentType = "application/octet-stream"
-$context.Response.AddHeader("Content-disposition", "attachment; filename=" + $file)
-$fs.CopyTo($context.Response.OutputStream)
-$context.Response.StatusCode = 200
-$context.Response.StatusDescription = "Ok"
-Write-Host -ForegroundColor Magenta "<< $($context.Response.StatusCode) $($context.Response.StatusDescription)"
-$context.Response.OutputStream.Close()
-$fs.Dispose()
-$http.Stop()
-$http.Close()
-$http.Dispose()
-Pop-Location
-
-if ($Debug) { Read-Host }
